ad917cba71faadc4a312bc9e1022d32bdce566e7
liao <liaoyongchao@outlook.com>
2013-08-25 22:29:05 +0800

    v0.1
    v0.1

---
+++ PhoenixLighting.DAL/BaseSqlOperation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoenixLighting.DAL
+{
+    public class BaseSqlOperation<T> where T:class
+    {
+        //基本操作
+        public T AddEntity(T Entity)
+        {
+            return Entity;
+        }
+
+        public bool Update(T Entity)
+        {
+            return true || false;
+        }
+
+        public bool InsertEntity(T Entity)
+        {
+            return true || false;
+        }
+
+        public bool DelEntity(T Entity)
+        {
+            return true || false;
+        }
+
+        //查询
+
+        //分页
+
+    }
+}
---
+++ PhoenixLighting.Model/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoenixLighting.Model
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public int Power { get; set; }
+        public int LightEffciency { get; set; }
+        public int Color { get; set; }
+        public string Function { get; set; }
+        public float Length { get; set; }
+        public float Diameter { get; set; }
+        public bool CE { get; set; }
+        public bool ROHS { get; set; }
+
+        public string Advantages { get; set; }
+        public string Specifications { get; set; }
+    }
+}
